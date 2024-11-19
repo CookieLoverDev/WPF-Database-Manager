c4f3b8ea434af2110ffcd154ee72dd6787096709
CookieLoverDev <ukhodjamov@gmail.com>
2024-11-19 10:17:36 +0100

    More visually appealing and some preventice code implemented

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -50,27 +50,41 @@ namespace Text_Editor
             _role = role.Text;
             _description = description.Text;
 
-            using (var connection = new SQLiteConnection(_connection))
+            if ((_name == string.Empty) || (_surname == string.Empty) || (_email == string.Empty) || (_role == string.Empty) || (_description == string.Empty))
             {
-                connection.Open();
-                string query = $"INSERT INTO info (Name, Surname, Email, Role, Description) VALUES (@Name, @Surname, @Email, @Role, @Description)";
-                using (var command = new SQLiteCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@Name", _name);
-                    command.Parameters.AddWithValue("@Surname", _surname);
-                    command.Parameters.AddWithValue("@Email", _email);
-                    command.Parameters.AddWithValue("@Role", _role);
-                    command.Parameters.AddWithValue("@Description", _description);
-                    command.ExecuteNonQuery();
-                }
+                MessageBox.Show("Please, fill out every form", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+                return;
             }
 
-            MessageBox.Show("File is succesfully saved!");
-            fname.Clear();
-            surname.Clear();
-            email.Clear();
-            role.Clear();
-            description.Clear();
+            try
+            {
+                using (var connection = new SQLiteConnection(_connection))
+                {
+                    connection.Open();
+                    string query = $"INSERT INTO info (Name, Surname, Email, Role, Description) VALUES (@Name, @Surname, @Email, @Role, @Description)";
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", _name);
+                        command.Parameters.AddWithValue("@Surname", _surname);
+                        command.Parameters.AddWithValue("@Email", _email);
+                        command.Parameters.AddWithValue("@Role", _role);
+                        command.Parameters.AddWithValue("@Description", _description);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("File is succesfully saved!");
+                fname.Clear();
+                surname.Clear();
+                email.Clear();
+                role.Clear();
+                description.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Entries were not saved in the database! Following error has occured {ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void InitializeDatabase()
